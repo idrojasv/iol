c5d47ab524cdb7f9adbca01351e18804eff5b785
idrojasv <ivanrojas@iMac-de-Ivan.local>
2017-03-07 11:17:23 -0500

    Configuracion Domain MApping

--- wp-content/sunrise.php
+++ wp-content/sunrise.php
@@ -1,37 +1,11 @@
 <?php
-if ( !defined( 'SUNRISE_LOADED' ) )
-	define( 'SUNRISE_LOADED', 1 );
+$sunrises = array(
+	"dm_sunrise" => defined("DM_CUSTOM_SUNRISE") ? DM_CUSTOM_SUNRISE : ( defined( "WP_PLUGIN_DIR" ) ?  rtrim( WP_PLUGIN_DIR, '/\\' ) . "/domain-mapping/inc/sunrise.php" : dirname( __FILE__ ) .  "/plugins/domain-mapping/inc/sunrise.php" ),
+	"md_sunrise" => defined("MD_CUSTOM_SUNRISE") ? MD_CUSTOM_SUNRISE : ( defined( "WP_PLUGIN_DIR" ) ?  rtrim( WP_PLUGIN_DIR, '/\\' ) . "/multi-domains/inc/sunrise.php" : dirname( __FILE__ ) .  "/plugins/multi-domains/inc/sunrise.php" )
+);
 
-if ( defined( 'COOKIE_DOMAIN' ) ) {
-	die( 'The constant "COOKIE_DOMAIN" is defined (probably in wp-config.php). Please remove or comment out that define() line.' );
-}
-
-$wpdb->dmtable = $wpdb->base_prefix . 'domain_mapping';
-$dm_domain = $_SERVER[ 'HTTP_HOST' ];
-
-if( ( $nowww = preg_replace( '|^www\.|', '', $dm_domain ) ) != $dm_domain )
-	$where = $wpdb->prepare( 'domain IN (%s,%s)', $dm_domain, $nowww );
-else
-	$where = $wpdb->prepare( 'domain = %s', $dm_domain );
-
-$wpdb->suppress_errors();
-$domain_mapping_id = $wpdb->get_var( "SELECT blog_id FROM {$wpdb->dmtable} WHERE {$where} ORDER BY CHAR_LENGTH(domain) DESC LIMIT 1" );
-$wpdb->suppress_errors( false );
-if( $domain_mapping_id ) {
-	$current_blog = $wpdb->get_row("SELECT * FROM {$wpdb->blogs} WHERE blog_id = '$domain_mapping_id' LIMIT 1");
-	$current_blog->domain = $dm_domain;
-	$current_blog->path = '/';
-	$blog_id = $domain_mapping_id;
-	$site_id = $current_blog->site_id;
-
-	define( 'COOKIE_DOMAIN', $dm_domain );
-
-	$current_site = $wpdb->get_row( "SELECT * from {$wpdb->site} WHERE id = '{$current_blog->site_id}' LIMIT 0,1" );
-	$current_site->blog_id = $wpdb->get_var( "SELECT blog_id FROM {$wpdb->blogs} WHERE domain='{$current_site->domain}' AND path='{$current_site->path}'" );
-	if ( function_exists( 'get_site_option' ) )
-		$current_site->site_name = get_site_option( 'site_name' );
-	elseif ( function_exists( 'get_current_site_name' ) )
-		$current_site = get_current_site_name( $current_site );
-
-	define( 'DOMAIN_MAPPING', 1 );
+foreach( $sunrises as $sunrise ){
+	if( is_readable( $sunrise ) ){
+		include $sunrise;
+	}
 }
